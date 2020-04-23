980ee53274d13c182084f67c2fbfa90ae0a7a1d2
shouko <imshouko@gmail.com>
2020-04-24 01:07:31 +0900

    Fix parsing of response header

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ const getTokenByStationId = async (stationId, defaultAreaId) => {
     transform,
   }).then(({ headers }) => ({
     authToken: headers[prependHeaderPrefix('authtoken', true)],
-    keyOffset: parseInt(prependHeaderPrefix('keyoffset', true), 10),
-    keyLength: parseInt(prependHeaderPrefix('keylength', true), 10),
+    keyOffset: parseInt(headers[prependHeaderPrefix('keyoffset', true)], 10),
+    keyLength: parseInt(headers[prependHeaderPrefix('keylength', true)], 10),
   })).then(({ authToken, keyOffset, keyLength }) => rp({
     uri: `${apiEndpoint}/auth2`,
     headers: {
