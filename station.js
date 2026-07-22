76e67c28f9f260c859e9a57f83c8e39d460f53a3
shouko <imshouko@gmail.com>
2026-07-22 23:56:01 +0900

    Replace JSDOM with XMLDOM

--- station.js
+++ station.js
@@ -1,34 +1,33 @@
-const { JSDOM } = require('jsdom');
+const { DOMParser } = require('@xmldom/xmldom');
 const config = require('./config');
 const areas = require('./constants/areas');
 const { fetchText } = require('./http');
 
-const areaToStations = {};
-const stations = {};
+let ready = false;
+const stationToAreas = new Map();
+
+const buildMappings = async () => Promise.all(areas.map((a) => a.id).map(async (areaId) => {
+  const res = await fetchText(
+    `${config.get('metadataEndpoint')}/station/list/${areaId}.xml`,
+  );
+  const document = new DOMParser().parseFromString(res, 'text/xml');
+  const stationIds = [...document.getElementsByTagName('station')].map(s => {
+    const id = [...s.children].find(({tagName}) => tagName === 'id');
+    return id?.textContent.trim();
+  }).filter(Boolean);
+
+  stationIds.forEach((sid) => {
+    if (!stationToAreas.has(sid)) stationToAreas.set(sid, []);
+    stationToAreas.get(sid).push(areaId);
+  });
+})).then(() => {
+  ready = true;
+});
 
 const getStationAvailableAreas = async (stationId) => {
-  if (stations[stationId]) return stations[stationId].areas;
-  await Promise.all(areas.map((a) => a.id).map(async (areaId) => {
-    const res = await fetchText(
-      `${config.get('metadataEndpoint')}/station/list/${areaId}.xml`,
-    );
-    const { document } = new JSDOM(res).window;
-    const stationIds = Array.prototype.map.call(
-      document.querySelectorAll('station > id'),
-      (n) => n.textContent,
-    );
-    areaToStations[areaId] = stationIds;
-    stationIds.forEach((sid) => {
-      if (!stations[sid]) {
-        stations[sid] = {
-          areas: [],
-        };
-      }
-      stations[sid].areas.push(areaId);
-    });
-  }));
-  if (!stations[stationId]) throw new Error('INVALID_STATION_ID');
-  return stations[stationId].areas;
+  if (!ready) await buildMappings();
+  if (!stationToAreas.has(stationId)) throw new Error('INVALID_STATION_ID');
+  return stationToAreas.get(stationId);
 };
 
 module.exports = {
